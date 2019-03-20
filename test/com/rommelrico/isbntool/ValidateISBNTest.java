01ab41605284fc2d7f54f02db0243b2fa31bfdd4
Rommel Rico <me@rommelrico.com>
2019-03-20 07:10:09 -0700

    A more complete test (won't compile)

--- test/com/rommelrico/isbntool/ValidateISBNTest.java
+++ test/com/rommelrico/isbntool/ValidateISBNTest.java
@@ -8,12 +8,16 @@ import static org.junit.jupiter.api.Assertions.*;
 Rules of TDD:
 1. Test the expected outcome of an example.
  -> Valid ISBN, Invalid ISBN
+2. Don't pre-judge design... let your tests drive it.
+ -> Don't assume ISBNs have to be Strings.
  */
 class ValidateISBNTest {
 
     @Test
     public void checkValidISBN() {
         ValidateISBN validator = new ValidateISBN();
+        boolean result = validator.checkISBN(0140449116);
+        assertTrue(result);
     }
 
 }
