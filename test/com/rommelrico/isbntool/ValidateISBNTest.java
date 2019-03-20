84c4fe5dcd15c87974cd74638b132587ac02e940
Rommel Rico <me@rommelrico.com>
2019-03-20 07:04:20 -0700

    Rules of TDD

--- test/com/rommelrico/isbntool/ValidateISBNTest.java
+++ test/com/rommelrico/isbntool/ValidateISBNTest.java
@@ -4,6 +4,11 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/*
+Rules of TDD:
+1. Test the expected outcome of an example.
+ -> Valid ISBN, Invalid ISBN
+ */
 class ValidateISBNTest {
 
     @Test
