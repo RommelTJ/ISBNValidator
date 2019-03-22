5caa21867ab228cbacf75ed515781580a16b56cd
Rommel Rico <me@rommelrico.com>
2019-03-22 06:30:15 -0700

    Naive checkISBN
    Mistake left on purpose

--- src/com/rommelrico/isbntool/ValidateISBN.java
+++ src/com/rommelrico/isbntool/ValidateISBN.java
@@ -3,7 +3,13 @@ package com.rommelrico.isbntool;
 class ValidateISBN {
 
     boolean checkISBN(String ISBN) {
-        return ISBN.equals("0140449116");
+        int total = 0;
+
+        for (int i = 0; i < 10; i++) {
+            total += ISBN.charAt(i) * (10 - i);
+        }
+
+        return total % 11 == 0;
     }
 
 }
