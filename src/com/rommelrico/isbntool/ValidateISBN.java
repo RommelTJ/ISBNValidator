007c7583403d96f1df5d157af2d04bf532008de9
Rommel Rico <me@rommelrico.com>
2019-03-24 17:42:35 -0700

    Fixing the code

--- src/com/rommelrico/isbntool/ValidateISBN.java
+++ src/com/rommelrico/isbntool/ValidateISBN.java
@@ -6,12 +6,17 @@ class ValidateISBN {
         if (ISBN.length() != 10) throw new NumberFormatException("Invalid Number Format");
         if (ISBN.matches(".*[a-zA-Z&&[^xX]]+.*")) throw new NumberFormatException("Invalid Number Format");
 
+        String upperISBN = ISBN.toUpperCase();
         int total = 0;
         for (int i = 0; i < 10; i++) {
-            if (i == 9 && !Character.isDigit(ISBN.charAt(i)) && ISBN.charAt(i) != 'X')
+            if (i == 9 && !Character.isDigit(upperISBN.charAt(i)) && upperISBN.charAt(i) != 'X')
                 throw new NumberFormatException("Invalid Number Format");
 
-            total += ISBN.charAt(i) * (10 - i);
+            int value;
+            if (upperISBN.charAt(i) == 'X') value = 10;
+            else value = Character.getNumericValue(ISBN.charAt(i));
+
+            total += value * (10 - i);
         }
 
         return total % 11 == 0;
