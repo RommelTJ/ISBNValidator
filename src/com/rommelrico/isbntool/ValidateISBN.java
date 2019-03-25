d0eb908e0f974528ea9558d1547ffcbfa6d05d9a
Rommel Rico <me@rommelrico.com>
2019-03-24 18:08:29 -0700

    Small code refactor

--- src/com/rommelrico/isbntool/ValidateISBN.java
+++ src/com/rommelrico/isbntool/ValidateISBN.java
@@ -3,24 +3,35 @@ package com.rommelrico.isbntool;
 class ValidateISBN {
 
     boolean checkISBN(String ISBN) {
-        if (ISBN.length() == 13) return true;
-        if (ISBN.length() != 10) throw new NumberFormatException("Invalid Number Format");
-        if (ISBN.matches(".*[a-zA-Z&&[^xX]]+.*")) throw new NumberFormatException("Invalid Number Format");
+        if (ISBN.length() != 10 && ISBN.length() != 13) throw new NumberFormatException("ISBN Numbers must be 10 or 13 characters.");
+        if (ISBN.matches(".*[a-zA-Z&&[^xX]]+.*")) throw new NumberFormatException("Invalid ISBN Format");
 
         String upperISBN = ISBN.toUpperCase();
+        if (ISBN.length() == 10) {
+            return validate10CharISBN(upperISBN);
+        } else {
+            return validate13CharISBN(upperISBN);
+        }
+    }
+
+    private boolean validate10CharISBN(String ISBN) {
         int total = 0;
         for (int i = 0; i < 10; i++) {
-            if (i == 9 && !Character.isDigit(upperISBN.charAt(i)) && upperISBN.charAt(i) != 'X')
+            if (i == 9 && !Character.isDigit(ISBN.charAt(i)) && ISBN.charAt(i) != 'X')
                 throw new NumberFormatException("Invalid Number Format");
 
             int value;
-            if (upperISBN.charAt(i) == 'X') value = 10;
+            if (ISBN.charAt(i) == 'X') value = 10;
             else value = Character.getNumericValue(ISBN.charAt(i));
 
             total += value * (10 - i);
         }
 
         return total % 11 == 0;
+    }
+
+    private boolean validate13CharISBN(String ISBN) {
+        return true;
     }
 
 }
