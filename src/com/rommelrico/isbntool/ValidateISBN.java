17d57c7ccf2857bf227ef1947a2d7d79647e91ff
Rommel Rico <me@rommelrico.com>
2019-03-25 19:36:02 -0700

    Refactoring constants

--- src/com/rommelrico/isbntool/ValidateISBN.java
+++ src/com/rommelrico/isbntool/ValidateISBN.java
@@ -2,12 +2,15 @@ package com.rommelrico.isbntool;
 
 class ValidateISBN {
 
+    public static final int SHORT_ISBN_LENGTH = 10;
+    public static final int LONG_ISBN_LENGTH = 13;
+
     boolean checkISBN(String ISBN) {
-        if (ISBN.length() != 10 && ISBN.length() != 13) throw new NumberFormatException("ISBN Numbers must be 10 or 13 characters.");
+        if (ISBN.length() != SHORT_ISBN_LENGTH && ISBN.length() != LONG_ISBN_LENGTH) throw new NumberFormatException("ISBN Numbers must be 10 or 13 characters.");
         if (ISBN.matches(".*[a-zA-Z&&[^xX]]+.*")) throw new NumberFormatException("Invalid ISBN Format");
 
         String upperISBN = ISBN.toUpperCase();
-        if (ISBN.length() == 10) {
+        if (ISBN.length() == SHORT_ISBN_LENGTH) {
             return validate10CharISBN(upperISBN);
         } else {
             return validate13CharISBN(upperISBN);
@@ -16,15 +19,15 @@ class ValidateISBN {
 
     private boolean validate10CharISBN(String ISBN) {
         int total = 0;
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < SHORT_ISBN_LENGTH; i++) {
             if (i == 9 && !Character.isDigit(ISBN.charAt(i)) && ISBN.charAt(i) != 'X')
                 throw new NumberFormatException("Invalid Number Format");
 
             int value;
-            if (ISBN.charAt(i) == 'X') value = 10;
+            if (ISBN.charAt(i) == 'X') value = SHORT_ISBN_LENGTH;
             else value = Character.getNumericValue(ISBN.charAt(i));
 
-            total += value * (10 - i);
+            total += value * (SHORT_ISBN_LENGTH - i);
         }
 
         return total % 11 == 0;
@@ -32,7 +35,7 @@ class ValidateISBN {
 
     private boolean validate13CharISBN(String ISBN) {
         int total = 0;
-        for (int i = 0; i < 13; i++) {
+        for (int i = 0; i < LONG_ISBN_LENGTH; i++) {
             if (i % 2 == 0) {
                 // Even
                 total += Character.getNumericValue(ISBN.charAt(i));
@@ -42,7 +45,7 @@ class ValidateISBN {
             }
         }
 
-        return total % 10 == 0;
+        return total % SHORT_ISBN_LENGTH == 0;
     }
 
 }
