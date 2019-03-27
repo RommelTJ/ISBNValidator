760d5405a76800c0b2ef6fda718827a41e41d29c
Rommel Rico <me@rommelrico.com>
2019-03-27 07:03:13 -0700

    Updating StockManager to generate a Locator Code

--- src/com/rommelrico/isbntool/StockManager.java
+++ src/com/rommelrico/isbntool/StockManager.java
@@ -2,7 +2,17 @@ package com.rommelrico.isbntool;
 
 class StockManager {
 
+    private ExternalISBNDataService service;
+
     String getLocatorCode(String ISBN) {
-        return "7396J4";
+        Book book = service.lookup(ISBN);
+        return ISBN.substring(ISBN.length() - 4) +
+               book.getAuthor().substring(0, 1) +
+               book.getTitle().split(" ").length;
     }
+
+    public void setService(ExternalISBNDataService service) {
+        this.service = service;
+    }
+
 }
