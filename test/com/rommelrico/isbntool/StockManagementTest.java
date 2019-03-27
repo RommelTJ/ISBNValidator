363ffc6258c91b85c1f992dda82422048b9c8b76
Rommel Rico <me@rommelrico.com>
2019-03-27 06:51:15 -0700

    Stock Management Tests

---
+++ test/com/rommelrico/isbntool/StockManagementTest.java
@@ -0,0 +1,13 @@
+package com.rommelrico.isbntool;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class StockManagementTest {
+
+    @Test
+    void testCanGetCorrectLocatorCode() {
+        fail();
+    }
+
+}
