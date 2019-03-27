ebf30238d642b3f3760ece0eab03cc1944c04726
Rommel Rico <me@rommelrico.com>
2019-03-27 07:11:24 -0700

    Adding a database service

--- src/com/rommelrico/isbntool/StockManager.java
+++ src/com/rommelrico/isbntool/StockManager.java
@@ -2,17 +2,22 @@ package com.rommelrico.isbntool;
 
 class StockManager {
 
-    private ExternalISBNDataService service;
+    private ExternalISBNDataService webService;
+    private ExternalISBNDataService dbService;
 
     String getLocatorCode(String ISBN) {
-        Book book = service.lookup(ISBN);
+        Book book = webService.lookup(ISBN);
         return ISBN.substring(ISBN.length() - 4) +
                book.getAuthor().substring(0, 1) +
                book.getTitle().split(" ").length;
     }
 
-    public void setService(ExternalISBNDataService service) {
-        this.service = service;
+    void setService(ExternalISBNDataService service) {
+        this.webService = service;
     }
 
+    void setDbService(ExternalISBNDataService dbService) {
+        this.dbService = dbService;
+    }
+    
 }
