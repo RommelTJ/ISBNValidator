cb3cc43ebf753fde19815adc86633ddf0cf7c5c6
Rommel Rico <me@rommelrico.com>
2019-03-28 07:18:07 -0700

    Implementing the web service test

--- src/test/java/com/rommelrico/isbntool/StockManagementTest.java
+++ src/test/java/com/rommelrico/isbntool/StockManagementTest.java
@@ -43,7 +43,24 @@ class StockManagementTest {
 
     @Test
     void webServiceIsUsedIfDataIsNotPresentInDatabase() {
-        fail();
+        ExternalISBNDataService mockDBService = mock(ExternalISBNDataService.class);
+        ExternalISBNDataService mockWebService = mock(ExternalISBNDataService.class);
+
+        when(mockDBService.lookup("0140177396")).thenReturn(null);
+        when(mockWebService.lookup("0140177396")).thenReturn(new Book("0140177396", "abc", "abc"));
+
+        StockManager stockManager = new StockManager();
+        stockManager.setWebService(mockWebService);
+        stockManager.setDBService(mockDBService);
+
+        String ISBN = "0140177396"; // Of Mice and Men
+        String locatorCode = stockManager.getLocatorCode(ISBN);
+
+        // Verify that lookup(ISBN) was called 1 times for mockDBService.
+        verify(mockDBService, times(1)).lookup(ISBN);
+
+        // Verify that the lookup(ISBN) was called 1 on the mockWebService.
+        verify(mockWebService, times(1)).lookup(ISBN);
     }
 
 }
