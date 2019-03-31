7c5c06ec393a9cb8c051df798cc59c13a982e4ae
Rommel Rico <me@rommelrico.com>
2019-03-31 08:12:25 -0700

    Spying out the interest rate

--- src/test/java/com/rommelrico/loans/RepaymentAmountTests.java
+++ src/test/java/com/rommelrico/loans/RepaymentAmountTests.java
@@ -1,6 +1,7 @@
 package com.rommelrico.loans;
 
 import org.junit.jupiter.api.*;
+import org.mockito.*;
 
 import java.math.*;
 
@@ -9,18 +10,21 @@ import static org.mockito.Mockito.*;
 
 class RepaymentAmountTests {
 
+    @Spy
+    LoanApplication loanApplication;
+
     @Test
     void test1YearLoanWholeDollars() {
-        LoanApplication loanApplication = new LoanApplication();
+        loanApplication = spy(new LoanApplication());
         loanApplication.setTermInMonths(12);
         loanApplication.setPrincipal(1200);
-        // Interest rate = 10%
+        doReturn(new BigDecimal(10)).when(loanApplication).getInterestRate();
         // Expected Repayment = 100
 
         LoanCalculatorController controller = new LoanCalculatorController();
         controller.processNewLoanApplication(loanApplication);
 
-        assertEquals(new BigDecimal(110), loanApplication.getRepayment());
+        assertEquals(new BigDecimal(100), loanApplication.getRepayment());
     }
 
     @Test
