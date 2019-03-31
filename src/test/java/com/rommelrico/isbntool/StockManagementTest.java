6aeb2d819fd4a7a4a177e6f924dd85b0db73e022
Rommel Rico <me@rommelrico.com>
2019-03-30 22:40:52 -0700

    Moving the stock manager to the setup

--- src/test/java/com/rommelrico/isbntool/StockManagementTest.java
+++ src/test/java/com/rommelrico/isbntool/StockManagementTest.java
@@ -9,11 +9,16 @@ class StockManagementTest {
 
     private ExternalISBNDataService mockDBService;
     private ExternalISBNDataService mockWebService;
+    private StockManager stockManager;
 
     @BeforeEach
     void setup() {
         mockDBService = mock(ExternalISBNDataService.class);
         mockWebService = mock(ExternalISBNDataService.class);
+
+        stockManager = new StockManager();
+        stockManager.setWebService(mockWebService);
+        stockManager.setDBService(mockDBService);
     }
 
     @Test
@@ -21,10 +26,6 @@ class StockManagementTest {
         String ISBN = "0140177396"; // Of Mice and Men
         when(mockWebService.lookup(ISBN)).thenReturn(new Book(ISBN, "Of Mice And Men", "John Steinbeck"));
         when(mockDBService.lookup(ISBN)).thenReturn(null);
-
-        StockManager stockManager = new StockManager();
-        stockManager.setWebService(mockWebService);
-        stockManager.setDBService(mockDBService);
 
         String locatorCode = stockManager.getLocatorCode(ISBN);
         assertEquals("7396J4", locatorCode); // Expected / Actual
@@ -34,12 +35,8 @@ class StockManagementTest {
     void databaseIsUsedIfDataIsPresent() {
         when(mockDBService.lookup("0140177396")).thenReturn(new Book("0140177396", "abc", "abc"));
 
-        StockManager stockManager = new StockManager();
-        stockManager.setWebService(mockWebService);
-        stockManager.setDBService(mockDBService);
-
         String ISBN = "0140177396"; // Of Mice and Men
-        String locatorCode = stockManager.getLocatorCode(ISBN);
+        stockManager.getLocatorCode(ISBN);
 
         // Verify that lookup(ISBN) was called 1 time for mockDBService.
         verify(mockDBService).lookup(ISBN);
@@ -53,12 +50,8 @@ class StockManagementTest {
         when(mockDBService.lookup("0140177396")).thenReturn(null);
         when(mockWebService.lookup("0140177396")).thenReturn(new Book("0140177396", "abc", "abc"));
 
-        StockManager stockManager = new StockManager();
-        stockManager.setWebService(mockWebService);
-        stockManager.setDBService(mockDBService);
-
         String ISBN = "0140177396"; // Of Mice and Men
-        String locatorCode = stockManager.getLocatorCode(ISBN);
+        stockManager.getLocatorCode(ISBN);
 
         // Verify that lookup(ISBN) was called 1 times for mockDBService.
         verify(mockDBService).lookup(ISBN);
