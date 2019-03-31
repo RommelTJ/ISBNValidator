6a472ad7f9550f6c9837d72054f47587d115fc33
Rommel Rico <me@rommelrico.com>
2019-03-31 07:50:01 -0700

    RepaymentAmountTests

---
+++ src/test/java/com/rommelrico/loans/RepaymentAmountTests.java
@@ -0,0 +1,9 @@
+package com.rommelrico.loans;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class RepaymentAmountTests {
+}
