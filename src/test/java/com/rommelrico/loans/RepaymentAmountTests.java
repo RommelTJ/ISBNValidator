53394f745021a2a6b28f7fce3367e582d0e88fe0
Rommel Rico <me@rommelrico.com>
2019-03-31 07:57:40 -0700

    Adding some examples

--- src/test/java/com/rommelrico/loans/RepaymentAmountTests.java
+++ src/test/java/com/rommelrico/loans/RepaymentAmountTests.java
@@ -9,16 +9,34 @@ class RepaymentAmountTests {
 
     @Test
     void test1YearLoanWholeDollars() {
+        LoanApplication loanApplication = new LoanApplication();
+        loanApplication.setTermInMonths(12);
+        loanApplication.setPrincipal(1200);
+        // Interest rate = 10%
+        // Expected Repayment = 100
+
         fail();
     }
 
     @Test
     void test2YearLoanWholeDollars() {
+        LoanApplication loanApplication = new LoanApplication();
+        loanApplication.setTermInMonths(24);
+        loanApplication.setPrincipal(1200);
+        // Interest rate = 10%
+        // Expected Repayment = 60
+
         fail();
     }
 
     @Test
     void test5YearLoanWithRounding() {
+        LoanApplication loanApplication = new LoanApplication();
+        loanApplication.setTermInMonths(60);
+        loanApplication.setPrincipal(5000);
+        // Interest rate = 6.5%
+        // Expected Repayment = 111
+
         fail();
     }
 
