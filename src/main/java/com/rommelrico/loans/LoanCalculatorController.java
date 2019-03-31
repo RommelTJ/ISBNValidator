feffd54cc227a73051f31b7eaa71dc76acd1d9ce
Rommel Rico <me@rommelrico.com>
2019-03-31 08:23:42 -0700

    Setter methods used for testing only

--- src/main/java/com/rommelrico/loans/LoanCalculatorController.java
+++ src/main/java/com/rommelrico/loans/LoanCalculatorController.java
@@ -53,7 +53,20 @@ public class LoanCalculatorController {
 		mailSender.send(message);
 		
 		return new ModelAndView("requestAccepted");
-	} 
-	
-	
+	}
+
+	// Setter methods used for testing only
+
+	public void setData(LoanRepository data) {
+		this.data = data;
+	}
+
+	public void setMailSender(JavaMailSender mailSender) {
+		this.mailSender = mailSender;
+	}
+
+	public void setRestTemplate(RestTemplate restTemplate) {
+		this.restTemplate = restTemplate;
+	}
+
 }
